2e24826e007029177b84cd73b08243c237778371
Phantomlsh <phantomlsh@outlook.com>
2021-04-04 08:39:39 +0800

    update affair page

--- src/views/Affair.vue
+++ src/views/Affair.vue
@@ -6,7 +6,7 @@
         <code>{{ id }}</code>
         <div class="buttons mt-3" v-if="affair">
           <button class="button is-small is-primary" :class="{ 'is-loading': loading }" @click="submit">提交事务信息</button>
-          <button class="button is-small is-danger" :class="{ 'is-loading': loading }" @click="remove">删除事务</button>
+          <button class="button is-small is-danger" :class="{ 'is-loading': loading }" @click="remove" v-if="route.params.id != 'NEW'">删除事务</button>
           <button class="button is-small is-info" @click="msg">发布消息</button>
         </div>
       </div>
@@ -21,7 +21,7 @@
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed, watch } from 'vue'
 import { affairs, token } from '../plugins/state.js'
 import axios from '../plugins/axios.js'
 import { md5 } from '../plugins/convention.js'
@@ -31,13 +31,16 @@ import AffairInfo from '../components/AffairInfo.vue'
 import AffairPreview from '../components/AffairPreview.vue'
 
 ref: affair = null
-ref: data = null
+ref: data = []
 ref: loading = false
 const id = computed(() => route.params.id == 'NEW' ? md5(Math.random()) : route.params.id)
 const title = computed(() => route.params.id == 'NEW'
   ? '创建新事务'
   : affair ? affair.title : '正在载入...'
 )
+watch(affairs, v => {
+  if (route.params.id != 'NEW' && !v[route.params.id]) setTimeout(window.close, 2000)
+})
 
 const catchErr = async e => {
   console.log(e)
@@ -69,23 +72,59 @@ if (route.params.id != 'NEW') getAffair()
 else affair = {}
 
 async function submit () {
+  if (!affair.title || !affair.groups || !affair.content) {
+    Swal.fire('错误', '标题、用户组、事务内容不能为空', 'error')
+    return
+  }
   const a = {
     id: id.value,
     title: affair.title,
-    groups: affair.groups.join(','),
+    groups: affair.groups,
     content: affair.content,
-    refs: affair.refs
+    refs: affair.refs,
   }
   if (affair.start) a.start = affair.start
   if (affair.end) a.end = affair.end
   if (affair.duration) a.duration = affair.duration
   if (affair.anonymous) a.anonymous = affair.anonymous
+  if (affair.preset) a.preset = affair.preset
+  if (affair.params) a.params = affair.params
   const vars = jsyaml.load(affair.variables)
   for (const k in vars) a['V:' + k] = vars[k]
   loading = true
   await axios.post('/affair', a, token())
-    .then(() => { Swal.fire('成功', '', 'success') })
+    .then(() => {
+      Swal.fire('成功', '', 'success')
+      affairs.value[id.value] = affair.title
+      route.params.id = id.value
+    })
     .catch(catchErr)
+  loading = false
+}
+
+async function remove () {
+  if (!data || !affair) return
+  const res = await Swal.fire({
+    title: '危险操作',
+    html: `删除事务${affair.title}?<br>事务数据也会被删除`,
+    icon: 'warning',
+    focusCancel: true,
+    confirmButtonText: '确定',
+    showCancelButton: true,
+    cancelButtonText: '取消'
+  })
+  if (!res.isConfirmed) return
+  loading = true
+  try {
+    for (const d of data) {
+      await axios.delete('/data?data=' + d, token())
+    }
+    await axios.delete('/affair?affair=' + id.value, token())
+    delete affairs.value[id.value]
+    Swal.fire('删除事务成功', '', 'success')
+  } catch (e) {
+    catchErr(e)
+  }
   loading = false
 }
 
