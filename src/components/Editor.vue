5fb668e42be8453bfddbd860339b2d89469c505a
Phantomlsh <phantomlsh@outlook.com>
2021-04-17 19:29:42 +0800

    Update piece
    Complete basic piece information collection

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,7 +1,8 @@
 <script>
 import { compile, ref } from 'vue'
 
-import Piece from './Piece.vue'
+import { PS } from '../plugins/state.js'
+import Piece from '../pieces/Piece.vue'
 
 function onError (e) {
   Swal.fire('错误', '事务信息错误', 'error')
@@ -9,7 +10,6 @@ function onError (e) {
 }
 
 export default {
-  ref: 'editor',
   props: ['modelValue'],
   emits: ['update:modelValue'],
   components: {
@@ -32,21 +32,37 @@ export default {
         const id = rp.match(/piece-start="(.*?)"/)[1]
         const regex = new RegExp(`<piece id="${id}"(.*?)/>`)
         const m = innerText.value.match(regex)
-        const p = m ? m[0] : ''
+        const p = (m && id[0] == '_') ? m[0] : ''
         res = res.replace(rp, p)
       }
       return res
+    }
+
+    function collect (html) {
+      // check exist
+      for (const p in PS.value) {
+        const regex = new RegExp(`<piece id="${p}"(.*?)/>`)
+        if (!html.match(regex)) delete PS.value[p]
+      }
+      // find all model="_piece"
+      const raw = html.match(/model="_(.*?)"/g) || []
+      for (const rp of raw) {
+        const id = rp.substr(7, rp.length - 8)
+        PS.value[id] = { T: 'simple' }
+      }
     }
 
     function blur () {
       let html = document.getElementById('editor').innerHTML
       html = clean(html)
       html = recover(html)
+      collect(html)
       ctx.emit('update:modelValue', html)
       innerText.value = html
     }
     
     const h = compile(`<div id="editor" class="content editor" contenteditable="true" @blur="blur">${innerText.value}</div>`, { onError })
+    collect(innerText.value)
     return (_ctx, _cache) => h({ blur, data }, _cache)
   }
 }
---
+++ src/pieces/index.js
@@ -0,0 +1,4 @@
+export const type = {
+  test: '测试组件',
+  enroll: '选课组件'
+}
