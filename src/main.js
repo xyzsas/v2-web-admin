2824d356d7491d573d504a6b4c45d1bc2915cfcc
Layzzi <Shadow_sock@icloud.com>
2021-08-30 12:27:40 +0800

    fix version

--- src/main.js
+++ src/main.js
@@ -5,5 +5,5 @@ createApp(App).mount('#app')
 
 window.onbeforeunload = () => '确认离开管理页面？页面数据将不会保留！'
 
-window.localStorage['web-admin-version'] = (import.meta.env.VITE_VERSION || 'develop').substr(0, 7)
+window.localStorage['web-admin-version'] = (import.meta.env.VITE_VERSION || 'develop')
 console.log('web-admin-version: ' + import.meta.env.VITE_VERSION)
